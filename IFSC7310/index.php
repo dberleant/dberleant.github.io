eb340a6182489253cdc74d01cf72fc1465963c47
dberleant <53141291+dberleant@users.noreply.github.com>
2023-01-31 23:04:43 -0600

    Add files via upload

---
+++ IFSC7310/index.php
@@ -0,0 +1,54 @@
+UNDER CONSTRUCTION
+
+<!doctype html>
+
+<html>
+    <body>
+        <h5>IFSC 7310, HW2, due Thursday 2/2/2023</h5>
+        <p>Directions: Please answer the questions below, posting the result in a document on your website, with a link to it from your home page.
+        
+        <ol>
+            <li>Learning resources.
+                <ul>
+                    <li>Consider the videoconference study & discussion group (see link in the schedule on the syllabus). Try out the link. What did you see? What is the role of HTML on this site? What about the site would HTML not be capable of implementing?</li>
+                    <li>Access the Slack site for the course (see link in the schedule on the syllabus). Post a hello message on it. Give a potential<a href="https://en.wikipedia.org/wiki/Use_case">use case</a> about you using it. A Fowler style use case would be one reasonable way to do this. If you have any questions about this, feel free to seek assistance using Slack.
+                    <li>Analyze: What is the role of HTML in the Slack site? What about the site would HTML not be capable of implementing?
+                </ul>
+            <li>This course.
+                <ul>
+                    <li>How does the teaching style of this course compare to the teaching styles of the courses you have taken in the past at your previous university? Do you have any comments on that?
+                    <li>Recall hW1. What about it could be improved for next year's class? What about it is good and should be kept for next year's class?
+                </ul>
+                
+                
+                
+                
+                
+                #########################################################
+                
+            <li>A critique of a common approach to grading was presented in class, and a solution to be used in this course was described. If your undergraduate institution used a different grading method, does the critique apply to that method also? Explain. If your undergraduate institution used the same method that was described, describe a scenario in which the critique applies, but with some difference from the scenario used in the example. 
+                <ul>
+                    <li>Put your answer in a file named HW1.html on your website and add a link that goes to it on your home page. That way someone can go to your home page and click on the "HW1" link and get to your HW1.
+                    </li>
+                </ul>
+            <li>Build an HTML test or demo page, which contains examples of various HTML commands. This page tests your browser to see how it handles different HTML commands and if it handles them properly. The examples should explain themselves. For example, if you demo bolding, have the demo be something like
+                <br><b>The b command is for bolding</b> (this is one way to do it)
+                <br>rather than something that doesn't explain it, like
+                <br><b>This shows bolding, but doesn't explain anything</b> (this is not a good way to do it).
+                <ul>
+                    <li>Link to your test page from your HW1 page.
+                    <li>Demo at least 30 things on the page.
+                </ul>
+            <li>Evaluate the power of HTML in social media websites. 
+                <ul>
+                    <li>For the social media website of your choice, what needs of this social media website does HTML meet? What needs does it not meet? Explain.
+                    <li>Invent a new kind of social media. (i) What does it do? (ii) What needs of this new social media website does HTML meet? (iii) What needs does it not meet?
+                    <li>Put your answer in your HW1.html file.
+                </ul>
+            <li>Physical exercise is well know to have various benefits. We could do a 1-2 minute exercise set in the middle of class to refresh and get the blood circulating. Is this a good idea, too offbeat, not sure, or what do you think? Put this answer in your HW1.html file.
+        </ul>
+
+    </body>
+</html>
+
+
